17a20ba9e4ba3f239a780343fa1973ac315e375d
ashish <ashishmasih92g@gmail.com>
2025-05-15 23:55:08 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,20 @@ import Open from "./Open";
 const App = () => {
 let [isOpen,setIsOpen] = useState(false)
 
+function handleOverlayClick(e) {
+    // Only close modal if the overlay itself is clicked, not the modal content
+    if (e.target === e.currnetTarget) {
+      isClose();
+    }
+  }
 
   return (
-    <div>
+    <div  className="model-overlay" >
        
 <button onClick={()=>{
   setIsOpen(true)
 }}>Show Model</button>
+
 
 <Open 
 flag ={isOpen}
--- src/components/Open.jsx
+++ src/components/Open.jsx
@@ -13,7 +13,7 @@ setFlag(false)
 }
 
     return (
-        <div   className="model-overlay"  style={{border:"2px solid black", width :"300px" , padding:"20px" }} >
+        <div  className="modal-content" onClick={isClose}  style={{border:"2px solid black", width :"300px" , padding:"20px" }} >
         <button className="model-close " onClick={isClose} style={{backgroundColor:"red", border:"none",}} >Button</button>
 <p className="model-p">This is the content of Model</p>
         </div>
