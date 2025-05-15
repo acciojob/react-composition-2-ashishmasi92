e2c2b29f8aa60dc5ef8446c6bc6d9ab2690da45a
ashish <ashishmasih92g@gmail.com>
2025-05-15 23:43:48 +0530

    assignment

--- src/components/Open.jsx
+++ src/components/Open.jsx
@@ -13,9 +13,9 @@ setFlag(false)
 }
 
     return (
-        <div onClick={isClose} style={{border:"2px solid black", width :"300px" , padding:"20px" }} >
-        <button style={{backgroundColor:"red", border:"none",}} >Button</button>
-<p>This is the content of Model</p>
+        <div   className="model-overlay"  style={{border:"2px solid black", width :"300px" , padding:"20px" }} >
+        <button className="model-close " onClick={isClose} style={{backgroundColor:"red", border:"none",}} >Button</button>
+<p className="model-p">This is the content of Model</p>
         </div>
     )
 }
